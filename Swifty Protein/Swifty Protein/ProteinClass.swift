269f6dd070da4c99b8e1f651d22e3417a724d187
pdespres <pdespres@student.42.fr>
2018-02-15 12:11:04 +0100

    day 1

---
+++ Swifty Protein/Swifty Protein/ProteinClass.swift
@@ -0,0 +1,9 @@
+//
+//  ProteinClass.swift
+//  Swifty Protein
+//
+//  Created by Paul DESPRES on 2/15/18.
+//  Copyright © 2018 Paul DESPRES. All rights reserved.
+//
+
+import Foundation
---
+++ Swifty Protein/Swifty Protein/ProteinTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProteinTableViewCell.swift
+//  Swifty Protein
+//
+//  Created by Paul DESPRES on 2/15/18.
+//  Copyright © 2018 Paul DESPRES. All rights reserved.
+//
+
+import UIKit
+
+class ProteinTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
